c4a0f8c3513dfd65528f98edad768afa5ffa09de
Steven Mercer <stevenmercer9913@gmail.com>
2021-02-15 09:09:35 -0800

    card flip works

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../components/App.css';
 import axios from 'axios';
 
@@ -13,57 +13,79 @@ function shuffleCardsArray(array) {
   return array;
 }
 
-class App extends React.Component {
-    state = {
-        cards: [],
-        player1: '',
-        player2: '',
-        isPlayerOnesTurn: true
+const App = () => {
+
+    const deck = [];
+
+    const turnObject = {
+        isPlayerOnesTurn: true,
+        cardsTurnedOver: 0
     }
 
-    getCards = (e) => {
-        e.preventDefault()
+    const [cards, setCards] = useState(deck)
+    const [turn, setTurn] = useState(turnObject)
+
+    const getCards = () => {
+        console.log('hello');
         axios.get('http://localhost:3000/cards').then((response) => {
             shuffleCardsArray(response.data);
-            this.setState({
-                cards: response.data,
-            })
+            setCards(response.data)
         })
     }
 
-    cardFlip = (card, e) => {
-        // e.preventDefault()
-        // const toggle = card.isFlipped;
-        // console.log(card.isFlipped);
-        // this.setState({
-        //     card.isFlipped: !(toggle)
-        // })
+    const cardFlip = (card, id) => {
 
+        // console.log(card.status);
+        const newCard = card;
+        // const newId = id;
+        console.log(newCard.status);
+        if (newCard.status === "back") {
+            newCard.status = "front"
+        } else if (newCard.status === "front") {
+            newCard.status = "back"
+        };
+        console.log(newCard.status);
+        let newCardsArr = cards.map((card) => {
+            console.log(newCard._id);
+            // console.log(id);
+            if (card._id === newCard._id) {
+                return newCard
+            } else {
+                return card
+            }
+        })
+        // console.log(card.status);
+        console.log(newCardsArr);
+        setCards(newCardsArr)
     }
 
-
-    render = ()=> {
-        return (
-            <div>
-                <div className="header"></div>
-                <div className="sidebar">
-                    <button onClick={this.getCards}>
-                        Deal
-                    </button>
-                </div>
-                <div className="gameboard">
-                    <h1>Here we are</h1>
-                    {this.state.cards.map((card, id) => {
-                        console.log(card);
-                        console.log(card.isFlipped);
-                        return (
-                            <button className="eachCard" key={id} onClick={() =>this.cardFlip(card)}><img className="cardImage" src={card.image} alt={card.name}/>{card.name}</button>
-                        )}
-                    )}
-                </div>
+    return (
+        <div>
+            <div className="header"></div>
+            <div className="sidebar">
+                <button onClick={()=>getCards()}>
+                    Deal
+                </button>
             </div>
-        );
-    }
+            <div className="gameboard">
+                <h1>Here we are</h1>
+                {console.log(cards)}
+                {cards.map((card, id) => {
+                    // console.log(card);
+                    console.log(card.status);
+                    return (
+                        <button className="eachCard" key={id} onClick={()=>cardFlip(card, id)}>
+                            {(card.status === "front")?
+                            <div><img className="cardImage" src={card.image} alt={card.name}/>{card.name}</div>
+                            : (card.status === "back")?
+                            <img className="cardImage" src={card.back} alt={card.name}/>
+                            : <img className="hiddenCard" src={""} alt={card.name}/>}
+                        </button>
+                    )}
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
